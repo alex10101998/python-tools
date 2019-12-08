eaeb09498071db26933acc3920593e5bfe5c1a36
alex10101998 <33742806+alex10101998@users.noreply.github.com>
2019-12-08 09:19:53 +0530

    Add files via upload
    This is a simple machanger . This can be implemented by the module sub processes which let's you execute bash cmds using python

---
+++ machanger.py
@@ -0,0 +1,9 @@
+# !/usr/bin/env python
+import subprocess
+print("starting the script")
+
+subprocess.call("ifconfig wlan0 down",shell=True)
+subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:55",shell=True)
+subprocess.call("ifconfig wlan0 up",shell=True)
+print("ending the script")
+
