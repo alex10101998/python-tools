f5cd8751a58d3fa7645cc11eb43c407477026b74
Alakshendra sharma <33742806+alex10101998@users.noreply.github.com>
2019-12-10 00:45:13 +0530

    Update machanger.py

--- machanger.py
+++ machanger.py
@@ -1,9 +1,12 @@
-# !/usr/bin/env python
-import subprocess
-print("starting the script")
+# !/usr/bin/env python     
+import subprocess  #subprocess module help in executing the linux commands through python 
+print("starting the script")  # printing a message
 
-subprocess.call("ifconfig wlan0 down",shell=True)
-subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:55",shell=True)
-subprocess.call("ifconfig wlan0 up",shell=True)
-print("ending the script")
+ #subprocess.call("linux command", shell=True)
+  #shell = True means executing the command through shell and not directly.
+
+subprocess.call("ifconfig wlan0 down",shell=True)  # first we have to bring the interface down 
+subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:55",shell=True) # sepecifing the random mac address to the ether
+subprocess.call("ifconfig wlan0 up",shell=True) # bringing the interface again up
+print("ending the script") # end message 
 
